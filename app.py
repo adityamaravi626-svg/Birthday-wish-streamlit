46b9aff37a2edef8ba2dc11d1c4ca245becf8818
adityamaravi626-svg <adityamaravi626@gmail.com>
2026-02-12 15:48:15 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st 
+from PIL import Image
+import base64
+
+st.set_page_config(page_title ="Special Gift 🎉", page_icon = "🎂",layout = "centered")
+
+st.title('✨🎊 Happy Birthday! 🎊✨')
+
+with open("birthday.mp3",'rb') as f:
+    audio_bytes = f.read()
+audio_base64 = base64.b64encode(audio_bytes).decode()  
+audio_html = f"""
+<audio autoplay loop>
+   <source src = "data:audio/mp3;base64,{audio_base64}" type = "audio/mp3">
+   </audio>
+   """
+st.markdown(audio_html,unsafe_allow_html = True)
+image = Image.open("harry.png")
+st.image(image,caption = ' # A Good Friend ❤️',use_container_width=True)
+
+st.markdown("""
+ ## 🎂 Wishing you a best birthday ever!, Harleen.\n
+     Keep smiling and shining always ❤️.             
+            """)
+
+st.markdown('---')
+st.markdown("""
+### 🎊 'May this year bring you closer to your dreams'. 
+ """)
+
+st.balloons() 
